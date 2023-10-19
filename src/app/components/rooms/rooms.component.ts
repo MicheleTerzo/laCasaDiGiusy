723b456882ba8c74453827eb557210c210165ca1
Michele Terzo <micheleterzo98@gmail.com>
2023-10-19 18:53:14 +0200

    Room section

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent {
-
 }
